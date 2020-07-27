9acd2fdd1ee9527f6241b4d4859a7e58a6889dbf
openspace-vr <sheena.boone@gmail.com>
2020-07-27 02:43:47 -0400

    if len(username) < minlen is unnecessary if user and user_length are parameters passed to validate_user

--- validations.py
+++ validations.py
@@ -7,15 +7,16 @@ def vailidate_user(username, minlen):
         raise TypeError("username must be a string")
     if minlen < 1:
         raise ValueError("minlen must be at least 1".)
-
-        if len(username) < minlen:
-            return False
-        if not username.isalnum():
-            return False
-        # Usernames can't begin with a number
-        if username [0].isnumeric():
-            return False
-        return True
+        #if you already have the length of the username, there is no rease to perform
+        #this next line
+        #if len(username) < minlen:
+            #return False
+    if not username.isalnum():
+        return False
+    # Usernames can't begin with a number
+    if username [0].isnumeric():
+        return False
+    return True
 
 main()
 "Have the user enter a new username; Pass the length of the parameter"
